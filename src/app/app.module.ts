4ff6c4114f16367f5bfeef5016f573b2d2baa5f8
Cristian Brotto <cristian.brotto@gmail.com>
2017-08-13 15:37:08 -0700

    App UI and components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,31 +4,37 @@ import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
 import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
-import { HomePage } from '../pages/home/home';
+import { RedditsPage } from '../pages/reddits/reddits';
+import { SettingsPage } from '../pages/settings/settings';
+import { DetailsPage } from '../pages/details/details';
 import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+import { HttpModule } from '@angular/http';
+
 @NgModule({
   declarations: [
     MyApp,
     AboutPage,
-    ContactPage,
-    HomePage,
+    RedditsPage,
+    SettingsPage,
+    DetailsPage,
     TabsPage
   ],
   imports: [
     BrowserModule,
+    HttpModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
     AboutPage,
-    ContactPage,
-    HomePage,
+    RedditsPage,
+    SettingsPage,
+    DetailsPage,
     TabsPage
   ],
   providers: [
